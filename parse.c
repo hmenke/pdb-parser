2c6aa1a5125b4bb4b45cb9e7bc5d6b15875fcb38
Henri Menke <henri@icp.uni-stuttgart.de>
2013-11-19 16:14:56 +0100

    Values are now being read and communicated to populate_lattice(...). In populate_lattice() the values are joined (but not saved yet)

--- parse.c
+++ parse.c
@@ -28,11 +28,12 @@ typedef struct {
 } itp_atomtypes;
 
 typedef struct {
-	int n_particles;
-	pdb_ATOM* atom_pdb_array;
-	itp_atoms* atoms_itp_array;
-	int n_parameters;
-	itp_atomtypes* atomtypes_itp_array;
+	unsigned int pdb_n_particles;
+	pdb_ATOM* pdb_array_ATOM;
+	unsigned int itp_n_particles;
+	itp_atoms* itp_array_atoms;
+	unsigned int itp_n_parameters;
+	itp_atomtypes* itp_array_atomtypes;
 } particle_data;
 
 /* BEGIN CODE */
@@ -80,15 +81,15 @@ int pdb_parse_files(char* pdb_filename, char* itp_filename, particle_data* atom_
 		}
 		if ( strncmp(pdb_line,"ATOM",4) == 0) {
 			// read all ATOMs
-			galloc( (void**) &atom_data->atom_pdb_array , (atom_data->n_particles+1)*sizeof(pdb_ATOM) );
-			pdb_ATOM* a = &atom_data->atom_pdb_array[atom_data->n_particles];
+			galloc( (void**) &atom_data->pdb_array_ATOM , (atom_data->pdb_n_particles+1)*sizeof(pdb_ATOM) );
+			pdb_ATOM* a = &atom_data->pdb_array_ATOM[atom_data->pdb_n_particles];
 			// See http://deposit.rcsb.org/adit/docs/pdb_atom_format.html#ATOM for the meaning of the format string
 			sscanf(pdb_line,"ATOM %5d %*4s%*c%*3s%*c%*4d%*c %8f %8f %8f %*6f %*6f %*4s%*2s%*2s",&a->i,&a->x,&a->y,&a->z);
 #ifdef DEBUG
 			// Print all local variables
 			printf("ATOM i=%d x=%f y=%f z=%f\n",a->i,a->x,a->y,a->z);
 #endif
-			atom_data->n_particles++;
+			atom_data->pdb_n_particles++;
 		}
 	}
 	fclose(pdb_file);
@@ -98,7 +99,7 @@ int pdb_parse_files(char* pdb_filename, char* itp_filename, particle_data* atom_
 	FILE* itp_file;
 	if ((itp_file = fopen(itp_filename,"r")) == NULL) return pdb_ERROR;
 #ifdef DEBUG
-		printf("### Reading itp-file \"%s\" ###\n",itp_filename);
+	printf("### Reading itp-file \"%s\" ###\n",itp_filename);
 #endif	
 	while (fgets(itp_line, sizeof(itp_line), itp_file)) {
 		// get section
@@ -109,32 +110,69 @@ int pdb_parse_files(char* pdb_filename, char* itp_filename, particle_data* atom_
 		if (itp_line[0] != '[' && itp_line[0] != ';' && itp_line[0] != '\r' && itp_line[0] != '\n') {
 			if (strcmp(section,"atoms") == 0) {
 				// section [ atoms ]
-				itp_atoms a;
+				galloc( (void**) &atom_data->itp_array_atoms , (atom_data->itp_n_particles+1)*sizeof(pdb_ATOM) );
+				itp_atoms* a = &atom_data->itp_array_atoms[atom_data->itp_n_particles];
 				// FIXME: no source :( Reverse engineered from the itp-file
-				sscanf(itp_line," %d %2s %*d %*s %*s %*d %f %*f ; %*s %*f",&a.i,a.type,&a.charge);
+				sscanf(itp_line," %d %2s %*d %*s %*s %*d %f %*f ; %*s %*f",&a->i,a->type,&a->charge);
 #ifdef DEBUG
 				// Print all local variables
-				printf("[ atoms ] i=%d type=%s charge=%f\n",a.i,a.type,a.charge);
+				printf("[ atoms ] i=%d type=%s charge=%f\n",a->i,a->type,a->charge);
 #endif
+				atom_data->itp_n_particles++;
 			}
 			if (strcmp(section,"atomtypes") == 0) {
 				// section [ atomtypes ]
-				itp_atomtypes a;
+				galloc( (void**) &atom_data->itp_array_atomtypes , (atom_data->itp_n_parameters+1)*sizeof(pdb_ATOM) );
+				itp_atomtypes* a = &atom_data->itp_array_atomtypes[atom_data->itp_n_parameters];
 				// FIXME: no source :( Reverse engineered from the itp-file
-				sscanf(itp_line," %2s %*s %*f %*f %*c %f %f ; %*f %*f",a.type,&a.sigma,&a.epsilon);
+				sscanf(itp_line," %2s %*s %*f %*f %*c %f %f ; %*f %*f",a->type,&a->sigma,&a->epsilon);
 #ifdef DEBUG
 				// Print all local variables
-				printf("[ atomtypes ] name=%s sigma=%f epsilon=%f\n",a.type,a.sigma,a.epsilon);
+				printf("[ atomtypes ] name=%s sigma=%f epsilon=%f\n",a->type,a->sigma,a->epsilon);
 #endif
+				atom_data->itp_n_parameters++;
 			}
 		}
 	}
 	fclose(itp_file);
 
+	if (atom_data->pdb_n_particles != atom_data->itp_n_particles) return pdb_ERROR;
 	return pdb_SUCCESS;
 }
 
-int pdb_parse(char* pdb_filename, char* itp_filename, float* lattice) {
+int populate_lattice(float** lattice, particle_data* atom_data) {
+	/*
+	 * This routine will populate the lattice using the
+	 * values read from the pdb and itp files.
+	 * WARNING: It contains much logic and interpolation stuff!
+	 */
+#ifdef DEBUG
+	printf("pdb_n_particles=%u, itp_n_particles=%u, itp_n_parameters=%u\n",atom_data->pdb_n_particles,atom_data->itp_n_particles,atom_data->itp_n_parameters);
+#endif
+	// prototype for joining the arrays
+	for (unsigned int i = 0; i <= atom_data->pdb_n_particles-1; i++) {
+		pdb_ATOM* a = &atom_data->pdb_array_ATOM[i];
+		itp_atoms* b = NULL;
+		itp_atomtypes* c = NULL;
+		for (unsigned int j = 0; j <= atom_data->itp_n_particles-1; j++) {
+			b = &atom_data->itp_array_atoms[j];
+			if (a->i == b->i) {
+				for (unsigned int k = 0; k <= atom_data->itp_n_parameters-1; k++) {
+					c = &atom_data->itp_array_atomtypes[k];
+					if (strcmp(b->type,c->type) == 0) {
+						printf("i=%d x=%f y=%f z=%f type=%s charge=%f sigma=%f epsilon=%f\n",a->i,a->x,a->y,a->z,b->type,b->charge,c->sigma,c->epsilon);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	lattice = NULL;
+	return pdb_SUCCESS;
+}
+
+int pdb_parse(char* pdb_filename, char* itp_filename, float** lattice) {
 	/*
 	 * This is the main parsing routine, which is visible to the outside
 	 * through the header parse.h. It doesn't contain any logic and just
@@ -144,15 +182,16 @@ int pdb_parse(char* pdb_filename, char* itp_filename, float* lattice) {
 	/* BEGIN DEPLOY */
 
 	particle_data atom_data;
-	atom_data.n_particles = 0;
-	atom_data.atom_pdb_array = NULL;
-	atom_data.atoms_itp_array = NULL;
-	atom_data.n_parameters = 0;
-	atom_data.atomtypes_itp_array = NULL;
+	atom_data.pdb_n_particles = 0;
+	atom_data.pdb_array_ATOM = NULL;
+	atom_data.itp_n_particles = 0;
+	atom_data.itp_array_atoms = NULL;
+	atom_data.itp_n_parameters = 0;
+	atom_data.itp_array_atomtypes = NULL;
 
 	pdb_parse_files(pdb_filename, itp_filename,&atom_data);
 
-	lattice = NULL;
+	populate_lattice(lattice, &atom_data);
 
 	return pdb_SUCCESS;
 }
--- parse.h
+++ parse.h
@@ -2,6 +2,6 @@
 #define _PDB_PARSER_H
 
 /* Returns 0/1 if reading the files was successful/unsuccessful */
-int pdb_parse(char* pdb_filename, char* itp_filename, float* lattice);
+int pdb_parse(char* pdb_filename, char* itp_filename, float** lattice);
 
 #endif
